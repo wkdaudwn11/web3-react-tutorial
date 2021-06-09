c87bb1932ff9bf500a2bdebd733a64040d73d257
JangMyungJoo <wkdaudwn11@naver.com>
2021-06-09 11:03:48 +0900

    처음부터 다시 하기 위해 파일 다 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,7 @@
 import React from 'react';
-import Web3Provider from './components/web3/Web3Provider';
-import Connect from './components/web3/Connect';
-import Main from './components/main';
 
 const App = () => {
-	return (
-		<Web3Provider>
-			<Connect>
-				<Main />
-			</Connect>
-		</Web3Provider>
-	);
+	return <div>test</div>;
 };
 
 export default App;
